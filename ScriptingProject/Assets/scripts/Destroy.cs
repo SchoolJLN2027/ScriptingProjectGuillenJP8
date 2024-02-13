d9023dccfe7b39b08f32229ad53734c6440a670d
Julien Guillen <723506@student.dusd.net>
2024-02-13 15:09:34 -0800

    destroy

---
+++ ScriptingProject/Assets/scripts/Destroy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject other;
+    // Update is called once per frame
+    void Update()
+    {
+        //DestroyBasic
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+
+        //DestroyOther
+        if (Input.GetKey(KeyCode.Keypad0))
+        {
+            Destroy(other);
+        }
+
+        //DestroyComponent 
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
