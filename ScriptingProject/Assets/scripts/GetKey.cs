41b9c2cd548ede6b9e0bd9309661f2938e12d064
Julien Guillen <723506@student.dusd.net>
2024-02-22 14:12:42 -0800

    get key

---
+++ ScriptingProject/Assets/scripts/GetKey.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine
+public class GetKey : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downfx;
+    public Sprite upfx;
+    //public Sprite
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
