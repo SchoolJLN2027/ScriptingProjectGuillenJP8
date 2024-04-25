ae78326f8e3b99da02a584d71b8f5c2329f68222
Julien Guillen <723506@student.dusd.net>
2024-04-25 14:25:30 -0700

    arrays
    4/25/24

---
+++ ScriptingProject/Assets/scripts/Array.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    public GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for(int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player number "+i+" is named " + players[i].name);
+        }
+    }
+}
